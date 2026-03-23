cab9df3c3366058167bf60d90b635c418f06fcb1
Jasmine Keebler <cutechao111@gmail.com>
2026-03-23 16:50:24 -0400

    Update formulas.py
    I'm now adding the Midpoint calculation function to formulas.py

--- formulas.py
+++ formulas.py
@@ -1,3 +1,4 @@
+
 def calculate_slope(x1, y1, x2, y2):
     """Calculates the slope (m) between two points."""
     try:
@@ -5,3 +6,10 @@ def calculate_slope(x1, y1, x2, y2):
         return m
     except ZeroDivisionError:
         return "Undefined (Vertical Line)"
+
+
+def calculate_midpoint(x1, y1, x2, y2):
+    """Calculates the midpoint (xm, ym) between two points."""
+    xm = (x1 + x2) / 2
+    ym = (y1 + y2) / 2
+    return (xm, ym)
